c499aaa0c2e8e1b9b5a24616b4c8c48923d9ca59
Misaki Koonce <misaki.koonce@gmail.com>
2020-01-29 23:53:40 -0800

    Add AnimalShelterAPI, Rename the original project for AnimalShelterMVC

---
+++ AnimalShelterAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AnimalShelterAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
